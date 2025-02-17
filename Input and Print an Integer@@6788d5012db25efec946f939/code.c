9e838c518e2460525e3338e495c7c4cbf519d1dc
Gauri Bansal <bansalgauri08@gmail.com>
2025-02-18 01:14:33 +0530

    Solution to Input and Print an Integer

--- Input and Print an Integer@@6788d5012db25efec946f939/code.c
+++ Input and Print an Integer@@6788d5012db25efec946f939/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
     float num;
-    scanf("%f",&num);
-    printf("You entered: %.2f",num);
+    scanf("%d",&num);
+    printf("You entered: %.2d",num);
     return 0;
 }
